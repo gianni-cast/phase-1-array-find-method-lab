3abffdb06e7546940a08de120f61fae1471d01dd
Gianni Castellano <gianniccastellano@gmail.com>
2024-05-26 15:16:22 -0400

    complete array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const superbowlVictory = record.find(match => match.result === "W")
+    if (superbowlVictory) {
+        return superbowlVictory.year
+    }
+}
